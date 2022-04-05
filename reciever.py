f289f0bc7ee1757a3e88b89188074e6d356a2e2a
vojtasob <vojta.sob@gmail.com>
2022-04-05 12:09:30 +0200

    HASH of whole file added

--- reciever.py
+++ reciever.py
@@ -1,7 +1,9 @@
+import hashlib
 import socket
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
+file_hash_calculated = hashlib.sha256()
 
 sock = socket.socket(socket.AF_INET,  # Internet
                      socket.SOCK_DGRAM)  # UDP
@@ -20,9 +22,9 @@ if mystring.startswith("NAME="):
     data, addr = sock.recvfrom(1024)
     mystring = data.decode('UTF-8')
     if mystring.startswith("START"):
-        print("Here")
+        print("Start of file transfer!")
         WRITE_TO_FILE = True
-        with open("output.jpg", "wb") as f:
+        with open(fileName, "wb") as f:
             while WRITE_TO_FILE:
                 data, addr = sock.recvfrom(1024)
                 try:
@@ -32,4 +34,16 @@ if mystring.startswith("NAME="):
                         break
                 except ValueError:
                     print("Image data accepted")
+                file_hash_calculated.update(data)
                 f.write(data)
+
+    data, addr = sock.recvfrom(1024)
+    str = data.decode("UTF-8")
+    if str.startswith("HASH="):
+        strList = str.split("=")
+        file_hash_recieved = strList[1]
+        if file_hash_recieved == file_hash_calculated.hexdigest():
+            print("Hash is correct")
+            print("File was transferred successfully!")
+
+
--- sender.py
+++ sender.py
@@ -1,11 +1,14 @@
 import os
 import socket
+import hashlib
 
-UDP_IP = "127.0.0.1"
+UDP_IP = "127.0.0.1"  # "147.32.216.17"
 UDP_PORT = 5005
 MESSAGE = b"Karel"
 
 file_name = "input.jpg"
+
+file_hash = hashlib.sha256()
 
 sock = socket.socket(socket.AF_INET,  # Internet
                      socket.SOCK_DGRAM)  # UDP
@@ -17,9 +20,12 @@ sock.sendto(bytes("START", "utf-8"), (UDP_IP, UDP_PORT))
 current_size = 0
 with open(file_name, 'rb') as f:
     while current_size < file_size:
-        sock.sendto(f.read(1024), (UDP_IP, UDP_PORT))
+        data = f.read(1024)
+        file_hash.update(data)
+        sock.sendto(data, (UDP_IP, UDP_PORT))
         current_size += 1024
 
 sock.sendto(bytes("END", "utf-8"), (UDP_IP, UDP_PORT))
-
+sock.sendto(bytes("HASH=" + file_hash.hexdigest(), "utf-8"), (UDP_IP, UDP_PORT))
+print(file_hash.hexdigest())
 sock.close()
